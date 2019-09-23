54418519b0bd0f7019188dc5b3f0071ea7b334f9
unknown <satheesh@dreamstudio.in>
2019-09-23 14:38:32 +0530

    app

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'HomeController@index')->name('home');
+
 
 Auth::routes();
 
@@ -19,6 +19,7 @@ Auth::routes();
 
 Route::group(['middleware' => ['auth']], function () {
 
+    Route::get('/', 'HomeController@index')->name('home');
     Route::get('/home', 'HomeController@index')->name('home');
     Route::resource('company', 'CompanyController');
 });
